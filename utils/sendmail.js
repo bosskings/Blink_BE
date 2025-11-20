a0835af77c1a59c932824991fc0eec47a19ace68
bossKings <wizzyiyk@gmail.com>
2025-11-20 16:42:05 +0100

    testing email sending

--- utils/sendmail.js
+++ utils/sendmail.js
@@ -1,12 +1,37 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-    // Configuration uses your secure Railway environment variables
-    host: process.env.MAIL_HOST,
-    port: 587, // Standard port for secured SMTP
-    secure: false, // Use TLS
-    auth: {
-        user: process.env.MAIL_USER, // Your service username (often 'apikey')
-        pass: process.env.MAIL_PASS  // Your service password/API key
-    }
-});
+const sendEmail =(to, message, title) =>{
+
+    const transporter = nodemailer.createTransport({
+        // Configuration uses your secure Railway environment variables
+        host: process.env.MAIL_HOST,
+        port: 587, // Standard port for secured SMTP
+        secure: false, // Use TLS
+        auth: {
+            user: "FoodGrabafrica@gmail.com",
+            pass: process.env.MAIL_PASS  // Your service password/API key
+        }
+    });
+    
+    const mailOptions = {
+        from : "FoodGrab Africa <foodgrabafrica@gmail.com>",
+        to: to,
+        subject: title,
+        html: `<p> ${message} </p>`
+    };
+    
+    
+    transporter.sendMail(mailOptions, (err, info)=>{
+        if(err){
+            console.log("Error sending email", err);
+            
+        }else{
+            console.log('success',info.messageId );
+    
+        }
+    });
+
+}
+
+
+export default sendEmail
